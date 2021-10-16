406d265279d88eddf7c1f0779588a1b635db3cbb
zedosoad <joao.pires.queiroz@gmail.com>
2021-10-16 17:28:48 +0200

    Dudisco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dudisco';
+
+  imgsList = ['../assets/im1.jpg', '../assets/im2.jpg', '../assets/im3.jpg'];
+
+  constructor() { }
+
+  setBackgroundImgs(num: number) {
+    let styles = {
+      'background-image': 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("' + this.imgsList[num + 1] + '") , url("' + this.imgsList[num + 2] + '")',
+    };
+    return styles;
+  }
 }
